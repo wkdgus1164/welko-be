075733e7055e754c11bb522c4f40c94e2a3d2b52
wkdgus1164 <wkdgus1164@naver.com>
2022-06-09 02:27:40 +0900

    login fail message - remove result code

--- routes/member.js
+++ routes/member.js
@@ -30,7 +30,7 @@ router.get("/login", (req, res) => {
     if (error) throw error
 
     if (rows.length <= 0) {
-      res.status(500).send("로그인 정보를 확인해 주세요.")
+      res.send("로그인 정보를 확인해 주세요.")
     } else {
       res.send(rows[0].name + "님, 환영합니다.")
     }
