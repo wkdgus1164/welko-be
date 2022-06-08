4fd0c7be2f38d249346e2cf53c3e2c48e064e610
wkdgus1164 <wkdgus1164@naver.com>
2022-06-09 01:39:08 +0900

    login

--- routes/member.js
+++ routes/member.js
@@ -15,6 +15,32 @@ router.get("/", (req, res) => {
   })
 })
 
+router.get("/login", (req, res) => {
+  const email = req.query.email
+  const password = req.query.password
+
+  console.log(req.query)
+
+  connection.query(`
+      SELECT *
+      FROM member
+      WHERE email = '${email}'
+        AND password = '${password}'
+  `, (error, rows) => {
+    if (error) throw error
+
+    if (rows.length <= 0) {
+      res.status(500).send({
+        code: 500,
+        message: "요청하신 정보와 일치하는 사용자가 없습니다."
+      })
+    } else {
+      res.send(rows)
+    }
+
+  })
+})
+
 router.get("/:id", (req, res) => {
   const id = req.params.id
 
